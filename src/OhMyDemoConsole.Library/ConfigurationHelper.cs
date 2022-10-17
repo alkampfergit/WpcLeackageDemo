dca043f8cb1edc8aadd825ebc54665af50266c89
Gian Maria Ricci <alkampfer@nablasoft.com>
2022-10-17 18:44:29 +0200

    First commit

---
+++ src/OhMyDemoConsole.Library/ConfigurationHelper.cs
@@ -0,0 +1,26 @@
+﻿using Serilog;
+
+namespace OhMyDemoConsole.Library
+{
+    public static class ConfigurationHelper
+    {
+        public static string? FindOverrideParentConfig(string fileName)
+        {
+            var directoryToCheck = new DirectoryInfo(AppDomain.CurrentDomain.BaseDirectory).Parent;
+            Log.Debug("Starting to look override file {0} starting from directory {1}", fileName, AppDomain.CurrentDomain.BaseDirectory);
+            while (directoryToCheck != null)
+            {
+                string overrideFile = Path.Combine(directoryToCheck.FullName, fileName);
+                if (File.Exists(overrideFile))
+                {
+                    Log.Debug("Found override configuration file: {0}", overrideFile);
+                    return overrideFile;
+                }
+                directoryToCheck = directoryToCheck.Parent;
+            }
+
+            Log.Debug("No override configuration file {0} found starting from directory {1}", fileName, AppDomain.CurrentDomain.BaseDirectory);
+            return null;
+        }
+    }
+}
---
+++ src/OhMyDemoConsole/Program.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.Configuration;
+using Serilog;
+
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .CreateLogger();
+
+var configurationBuilder = new ConfigurationBuilder()
+  .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+
+IConfiguration Configuration = configurationBuilder.Build();
+
+var secret = Configuration.GetValue<string>("Secret");
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine($"My secret is: {secret}");
+Console.ReadKey();
